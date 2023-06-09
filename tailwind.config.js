351ac9f2529d248d7302a25d4a10d91541eb5f99
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-06-09 03:35:29 -0400

    Setup project

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      white: "#FCFDFE",
+      text: "#081521",
+      accent: "#070773",
+      primary: "#0D70D3",
+      secondary: "#03032B",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["winter"],
+  },
+};
