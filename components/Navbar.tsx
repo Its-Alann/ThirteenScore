f3be14f4f9a99df6930e5191104e52ea17a0907a
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-06-09 03:35:51 -0400

    Added navbart

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar border">
+      <a className="normal-case text-xl px-2 font-bold">ThirteenScore</a>
+    </div>
+  );
+};
+
+export default Navbar;
