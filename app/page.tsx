518dc3243c999eb3fe9265c061f3058a70f5791e
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-06-09 03:36:17 -0400

    Setting up main page

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Table from "@/components/Table";
+
+const page = () => {
+  return (
+    <div className="flex flex-col items-center h-screen mt-40">
+      <div>
+        <p className="font-bold text-4xl">Welcome to ThirteenScore</p>
+      </div>
+      <div className="text-xl">
+        Let&apos;s get you started with your 3-13 game
+      </div>
+      <div className="mt-20">
+        <div className=" text-lg">Enter the names of the players</div>
+        <div>
+          <Table />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
