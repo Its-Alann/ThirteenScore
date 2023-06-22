649337ed553d58ac9d1673b1bcff60204154d3f6
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-06-22 19:06:42 -0400

    Redirect when you click on the logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
 const Navbar = () => {
+  const [confirmNavigation, setConfirmNavigation] = useState(false);
+
+  useEffect(() => {
+    const handleBeforeUnload = (event: any) => {
+      if (window.location.pathname === "/game") {
+        event.preventDefault();
+        event.returnValue = ""; // Chrome requires a non-empty string value
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [confirmNavigation]);
+
+  const handleLogoClick = (event: any) => {
+    if (window.location.pathname === "/game") {
+      const userConfirmation = window.confirm(
+        "Are you sure you want to leave the game? You will lose all progress."
+      );
+
+      if (!userConfirmation) {
+        event.preventDefault();
+      }
+    }
+  };
+
   return (
-    <div className="navbar border">
-      <a className="normal-case text-xl px-2 font-bold">ThirteenScore</a>
+    <div className="navbar">
+      <Link
+        href="/"
+        className="normal-case text-xl px-2 font-bold"
+        onClick={handleLogoClick}
+      >
+        ThirteenScore
+      </Link>
     </div>
   );
 };
