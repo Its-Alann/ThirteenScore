04af40c4926d151a8a1d728791db68e30f137e74
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-06-22 19:07:22 -0400

    History and total score for each player

--- components/Scoreboard.tsx
+++ components/Scoreboard.tsx
@@ -16,18 +16,47 @@ import { usePlayerContext } from "../app/context/player";
 export function createData(
   player: string,
   score: number,
-  history: { round: number; score: number }[]
+  history: { round: number; score: number }[],
+  id: number
 ) {
   return {
     player,
     score,
     history,
+    id,
   };
 }
 
-function Row(props: { row: ReturnType<typeof createData>; round: number }) {
+function Row(props: {
+  row: ReturnType<typeof createData>;
+  round: number;
+  playerID: number;
+}) {
   const { row } = props;
   const [open, setOpen] = React.useState(false);
+  const { players, setPlayers } = usePlayerContext();
+  const [defaultScore, setDefaultScore] = React.useState(0);
+
+  const updateCurrentScore = (playerID: number, score: number) => {
+    players.map((player) => {
+      if (player.id == playerID) {
+        player.score = score;
+      }
+    });
+    setDefaultScore(0);
+  };
+
+  const calculateTotal = (playerID: number): number => {
+    const foundPlayer = players.find((player) => playerID === player.id);
+    if (foundPlayer) {
+      const totalScore = foundPlayer.history?.reduce(
+        (sum, entry) => sum + entry.score,
+        0
+      );
+      return totalScore || 0;
+    }
+    return 0;
+  };
 
   return (
     <React.Fragment>
@@ -41,18 +70,27 @@ function Row(props: { row: ReturnType<typeof createData>; round: number }) {
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </TableCell>
-        <TableCell component="th" scope="row">
+        <TableCell component="th" scope="row" align="right">
           {row.player}
         </TableCell>
         <TableCell align="right">
           <input
-            type="text"
+            type="number"
             placeholder=""
             className="input input-bordered input-sm w-14 max-w-xs"
-            defaultValue={row.score}
-            // onChange={(e: React.FormEvent<HTMLInputElement>) =>
-            //   changeNames(player.id, e.currentTarget.value)
-            // }
+            // defaultValue={defaultScore}
+            onChange={(e: React.FormEvent<HTMLInputElement>) =>
+              updateCurrentScore(props.playerID, e.currentTarget.valueAsNumber)
+            }
+          />
+        </TableCell>
+        <TableCell component="th" scope="row" align="right">
+          <input
+            type="number"
+            placeholder=""
+            className="input input-sm w-16 max-w-xs"
+            value={calculateTotal(props.playerID)}
+            disabled
           />
         </TableCell>
       </TableRow>
@@ -72,30 +110,31 @@ function Row(props: { row: ReturnType<typeof createData>; round: number }) {
                     </TableRow>
                   </TableHead>
 
-                  {props.round === 1 ? (
-                    <></>
-                  ) : (
-                    <TableBody>
-                      {row.history.map((historyRow) => (
-                        <TableRow key={historyRow.round}>
-                          <TableCell component="th" scope="row">
-                            {historyRow.round}
-                          </TableCell>
-                          <TableCell align="right">
-                            <input
-                              type="text"
-                              placeholder=""
-                              className="input input-bordered input-xs w-14 max-w-xs"
-                              defaultValue={historyRow.score}
-                              // onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                              //   changeNames(player.id, e.currentTarget.value)
-                              // }
-                            />
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  )}
+                  <TableBody>
+                    {players.map((player) => {
+                      if (player.id == props.playerID) {
+                        return player.history?.map((historyRow) => (
+                          <TableRow key={historyRow.round}>
+                            <TableCell component="th" scope="row">
+                              {historyRow.round}
+                            </TableCell>
+                            <TableCell align="right">
+                              <input
+                                type="number"
+                                placeholder=""
+                                className="input input-bordered input-xs w-14 max-w-xs"
+                                defaultValue={historyRow.score}
+                                disabled
+                                // onChange={(e: React.FormEvent<HTMLInputElement>) =>
+                                //   changeNames(player.id, e.currentTarget.value)
+                                // }
+                              />
+                            </TableCell>
+                          </TableRow>
+                        ));
+                      }
+                    })}
+                  </TableBody>
                 </Table>
               </Box>
             </div>
@@ -106,22 +145,40 @@ function Row(props: { row: ReturnType<typeof createData>; round: number }) {
   );
 }
 
-export default function CollapsibleTable(props: {
-  round: number;
-  history: { round: number; score: number }[];
-}) {
+export default function CollapsibleTable(props: { round: number }) {
   const { players, setPlayers } = usePlayerContext();
-  const rows = players.map((player) =>
-    createData(player.name, player.score, props.history)
-  );
+
+  // const sortedPlayers = players.sort((a, b) => a.score - b.score);
+
+  const rows = players.map((player) => {
+    console.log("player array", players);
+    return createData(
+      player.name,
+      player.score,
+      player.history || [],
+      player.id
+    );
+  });
 
   return (
-    <TableContainer component={Paper} sx={{ boxShadow: 0 }}>
+    <TableContainer
+      component={Paper}
+      sx={{ boxShadow: 0 }}
+      style={{ maxHeight: "400px", overflowY: "auto", minWidth: 600 }}
+    >
       <Table aria-label="collapsible table">
         <TableBody>
-          {rows.map((row) => (
-            <Row key={row.player} row={row} round={props.round} />
-          ))}
+          {rows.map((row) => {
+            console.log(row.id);
+            return (
+              <Row
+                key={row.id}
+                row={row}
+                round={props.round}
+                playerID={row.id}
+              />
+            );
+          })}
         </TableBody>
       </Table>
     </TableContainer>
